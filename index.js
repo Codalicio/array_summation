8e67c40da17558cdf60aef9754884e7dd57567a8
Amit Raj <rajamitnine@gmail.com>
2025-08-06 21:57:46 +0530

    code improved

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sum(arr, initialValue = 0) {
-  return arr.reduce(
+  let copyArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let otherThanDigit = arr[i];
+    if (typeof otherThanDigit !== "number") {
+      otherThanDigit = 0;
+    }
+    copyArr.push(otherThanDigit);
+  }
+  return copyArr.reduce(
     (accumulator, arrayItem) => accumulator + arrayItem,
     initialValue
   );
